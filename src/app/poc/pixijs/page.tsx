0d52526026e02f3a5e9bfcb6524f79f510fc35c5
Yanguk <yanguk@duck.com>
2024-08-25 17:55:57 +0900

    sample

--- src/app/poc/pixijs/page.tsx
+++ src/app/poc/pixijs/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, {
+  ChangeEventHandler,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
-import { BlurFilter, DisplayObject, TextStyle } from "pixi.js";
+import { BlurFilter, DisplayObject, Graphics, TextStyle } from "pixi.js";
 import { Stage, Container, Sprite, Text } from "@pixi/react";
 import { PocaBack, PocaCap, PocaWord } from "./pocas";
 import { InputFile } from "@/components/file-input";
+import { Button } from "@/components/ui/button";
 
 interface Draggable extends DisplayObject {
   data: any | null;
   dragging: boolean;
+}
+
+export function downloadURI(uri: string, name: string) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
 const useDrag = ({ x, y }: any) => {
@@ -45,23 +61,79 @@ const useDrag = ({ x, y }: any) => {
 function App() {
   const blurFilter = useMemo(() => new BlurFilter(2), []);
   const bunnyUrl = "https://pixijs.io/pixi-react/img/bunny.png";
+  const [image, setImage] = useState<string>("");
+
+  const [myselfImage, setMyselfImage] = useState<any>("");
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = function (e: any) {
+      const image = new Image();
+      image.src = e.target.result;
+      image.width = 650;
+
+      image.onload = function () {
+        setMyselfImage(image.src);
+      };
+    };
+
+    fileReader.readAsDataURL(file);
+  };
+
+  const ref = useRef<any>();
 
   const capBind = useDrag({ x: PocaCap.x, y: PocaCap.y });
   const wordBind = useDrag({ x: PocaWord.x, y: PocaWord.y });
+  const pictureBind = useDrag({ x: 0, y: 0 });
+
+  useEffect(() => {
+    if (ref.current) {
+      (async () => {})();
+    }
+  }, [ref]);
+
+  const handleOnClick = async () => {
+    const app = ref.current.app;
+
+    const graphics = new Graphics().beginFill(0xff0000).drawCircle(0, 0, 50);
+
+    const image = await app.renderer.plugins.extract.image(
+      app.stage,
+      "image/png",
+      1,
+    );
+
+    downloadURI(image.src, "poca.png");
+  };
 
   return (
     <>
       <h1>아직 작업중...</h1>
-      <InputFile />
+      <Button onClick={handleOnClick}>download</Button>
+      <InputFile onChange={onChange} />
 
       <br />
 
       <Stage
+        ref={ref}
         width={650}
         height={1004}
         options={{ background: 0xfee1c6 }}
         className="m-auto"
       >
+        {myselfImage && (
+          <Sprite
+            width={650}
+            height={1004}
+            image={myselfImage}
+            {...pictureBind}
+          />
+        )}
         <Sprite image={PocaBack.image} x={PocaBack.x} y={PocaBack.y} />
 
         <Sprite
